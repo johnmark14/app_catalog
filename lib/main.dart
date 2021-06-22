fc20bdf831d154827e9fe86c4eab18d8ab00a639
johnmark14 <johmarkgabrentina@gmail.com>
2021-06-22 13:57:53 +0800

    added new login page, moved files to pages folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/homePage",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/homePage": (context) => HomePage(),
+        "/loginPage": (context) => LoginPage(),
+      },
+    );
   }
 }
