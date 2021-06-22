efaae748c3f19b1c152c43050da0cb0b2761248e
johnmark14 <johmarkgabrentina@gmail.com>
2021-06-22 15:45:44 +0800

    Added login page details

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,8 +4,47 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text('Login Page'),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_art.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter your username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter your password", labelText: "Password"),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print("Button login is clicked!!");
+              },
+              child: Text("Login"))
+        ],
       ),
     );
   }
